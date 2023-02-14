9544fa65b3419ce33aaf7dc3dd17ddc6e343a50e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-14 16:53:12 +0530

    Added useEffect to Pre-fetch the Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,13 +7,18 @@ import { MovieType } from "./Store/Movie.types";
 function App() {
   const [getMovies, setMovies] = useState<MovieType[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<null | string>(null);
 
   const fetchMovieHandler = async () => {
     try {
       //? Setting movies
       setMovies([]);
+      setError(null);
       setLoading(true);
       const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went Wrong");
+      }
       const data = await response.json();
       const result = data.results;
       result.map((data: any) => {
@@ -27,22 +32,36 @@ function App() {
           return [...prevMovies, movieData];
         });
       });
-      setLoading(false);
     } catch (e) {
-      console.log(e);
+      setError((e as DOMException).message);
     }
+    setLoading(false);
   };
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
+
+  let Content = (
+    <h4 className="loading-message">Loading it may take a while...</h4>
+  );
+
+  if (error) {
+    Content = <h4 className="error-message">{error}</h4>;
+  }
+  if (!loading && getMovies.length > 0) {
+    Content = <MoviesList movies={getMovies} />;
+  }
+  if (!loading && getMovies.length === 0 && !error) {
+    Content = <h4 className="error-message">Found no movies.</h4>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section className="movies-section">
-        {loading && <h4>Loading it may take a while...</h4>}
-        {!loading && getMovies.length > 0 && <MoviesList movies={getMovies} />}
-        {!loading && getMovies.length === 0 && <h4>Found no movies.</h4>}
-      </section>
+      <section className="movies-section">{Content}</section>
     </React.Fragment>
   );
 }
