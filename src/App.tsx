ef100b42125011c401a32e4d3975cf567426674e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-14 15:21:28 +0530

    Fetching data from backend using API Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import { MovieType } from "./Store/Movie.types";
 
 function App() {
   const dummyMovies = [
@@ -31,13 +32,39 @@ function App() {
     },
   ];
 
+  const [getMovies, setMovies] = useState<MovieType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchMovieHandler = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      const result = data.results;
+      result.map((data: any) => {
+        const movieData: MovieType = {
+          id: data.episode_id,
+          title: data.title,
+          openingText: data.opening_crawl,
+          releaseDate: data.release_date,
+        };
+        setMovies((prevMovies) => {
+          return [...prevMovies, movieData];
+        });
+      });
+      setLoading(false)
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section className="movies-section">
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={getMovies} />
       </section>
     </React.Fragment>
   );
